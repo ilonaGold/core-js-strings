2c7a2ba19f61b20d44ceef380194e10a6aab1236
Ilona Goldmane <songa17@inbox.lv>
2024-12-13 00:38:53 +0200

    feat: implement getStringLength function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,8 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(str) {
+  return typeof str === 'string' ? str.length : 0;
 }
 
 /**
