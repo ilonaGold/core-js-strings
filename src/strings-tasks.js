acb9462c519a53260bd0ad5d62b4d4cc44bb383f
Ilona Goldmane <songa17@inbox.lv>
2024-12-13 23:45:05 +0200

    feat: implement formatTime function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -240,8 +240,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const mm = String(minutes).padStart(2, '0');
+  const ss = String(seconds).padStart(2, '0');
+  return `${mm}:${ss}`;
 }
 
 /**
